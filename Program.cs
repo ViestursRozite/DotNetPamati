4cede38392bbb77bc42b2006d8a0cdadd277c78d
ViestursRozite <viesturs_mikus_rozite@inbox.lv>
2024-12-11 17:21:32 +0200

    up

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void MyFunctionName(string myVariableName)
+{
+    Console.WriteLine("Hello, World!");
+}
+
+MyFunctionName("");
